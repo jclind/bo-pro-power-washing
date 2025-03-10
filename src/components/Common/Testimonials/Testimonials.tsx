1d8e468689e3b0c158c2eee0a0f05bab0befa791
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-10 19:16:11 -0400

    feat(Testimonials): create and add testimonials component to home page

--- src/components/Common/Testimonials/Testimonials.tsx
+++ src/components/Common/Testimonials/Testimonials.tsx
@@ -1,7 +1,46 @@
 import React from 'react'
 import styles from './testimonials.module.scss'
+import { testimonialList } from './testimonialList'
+import { FaQuoteLeft, FaStar, FaRegStar } from 'react-icons/fa'
 const Testimonials = () => {
-  return <div>Testimonials</div>
+  const renderStars = rating => {
+    const maxStars = 5 // Maximum number of stars
+
+    return Array.from({ length: maxStars }, (_, index) =>
+      index < rating ? (
+        <FaStar key={index} className={styles.filledStar} />
+      ) : (
+        <FaRegStar key={index} className={styles.emptyStar} />
+      )
+    )
+  }
+
+  return (
+    <div className={styles.testimonials}>
+      <div className='page__inner page__horizontal-spacing-2xl'>
+        <div className={styles.content}>
+          <h1 className='section-title'>Customer Testimonials</h1>
+          <p className='section-paragraph'>
+            Don't just take our word for it, here's what our clients have to say
+            about our power washing.
+          </p>
+
+          <div className={styles.list}>
+            {testimonialList.map(t => (
+              <div className={`${styles.item} card`}>
+                <div className={styles.icon_container}>
+                  <FaQuoteLeft className={styles.icon} />
+                </div>
+                <p className={styles.quote}>"{t.text}"</p>
+                <div className={styles.rating}>{renderStars(t.rating)}</div>
+                <div className={styles.name}>- {t.name}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Testimonials
---
+++ src/components/Common/Testimonials/testimonialList.ts
@@ -0,0 +1,17 @@
+export const testimonialList = [
+  {
+    text: 'Sint tempor ea magna proident voluptate amet labore. Sint excepteur esse exercitation veniam elit minim proident quis cupidatat enim cupidatat minim tempor. Eu irure velit quis culpa eiusmod qui sint esse dolor minim est fugiat.',
+    name: 'John S.',
+    rating: '5',
+  },
+  {
+    text: 'Quis consequat dolor in consequat ut culpa consectetur non reprehenderit occaecat veniam sint nostrud. Aliquip in tempor incididunt quis voluptate nostrud qui culpa. Velit aliqua tempor do consequat ipsum nostrud ullamco in officia adipisicing sit deserunt esse. Et occaecat incididunt ullamco Lorem eu non qui.',
+    name: 'Sara H.',
+    rating: '5',
+  },
+  {
+    text: 'Enim cupidatat dolore Lorem duis non proident minim reprehenderit minim labore ipsum. Commodo ut exercitation eu eiusmod elit qui ullamco occaecat reprehenderit ea non irure. Id ad eiusmod nulla esse.',
+    name: 'James L.',
+    rating: '5',
+  },
+]
