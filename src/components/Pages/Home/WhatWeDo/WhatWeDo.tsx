4c8c394ab017c8404e67c506d2145c7f220f21a2
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-10 17:19:10 -0400

    feat(home | WhatWeDo): add mobile responsiveness for WhatWeDo componenet

--- src/components/Pages/Home/WhatWeDo/WhatWeDo.tsx
+++ src/components/Pages/Home/WhatWeDo/WhatWeDo.tsx
@@ -8,13 +8,23 @@ const WhatWeDo = () => {
     <div className={styles.what_we_do}>
       <div className='page__inner page__horizontal-spacing-3xl'>
         <div className={styles.content}>
-          <h1 className='section-title'>What We Do</h1>
-          <p className='section-paragraph'>
-            At Bo Pro Power Washing, we provide reliable, efficient, and
-            professional cleaning services to keep your home looking its best.
-            From driveways to siding, we offer a range of options to fit your
-            needs.
-          </p>
+          <div className={styles.text_section}>
+            <h1 className='section-title'>What We Do</h1>
+            <p className='section-paragraph'>
+              At Bo Pro Power Washing, we provide reliable, efficient, and
+              professional cleaning services to keep your home looking its best.
+              From driveways to siding, we offer a range of options to fit your
+              needs.
+            </p>
+            <div className={styles.button_container_desktop}>
+              <ButtonLink
+                text='See All Services'
+                link='/services'
+                color='tertiary'
+                size='large'
+              />
+            </div>
+          </div>
           <div className={styles.services}>
             {services.map(service => (
               <div className={`${styles.service} card`}>
@@ -28,12 +38,14 @@ const WhatWeDo = () => {
               </div>
             ))}
           </div>
-          <ButtonLink
-            text='See All Services'
-            link='/services'
-            color='tertiary'
-            size='large'
-          />
+          <div className={styles.button_container_mobile}>
+            <ButtonLink
+              text='See All Services'
+              link='/services'
+              color='tertiary'
+              size='large'
+            />
+          </div>
         </div>
       </div>
     </div>
