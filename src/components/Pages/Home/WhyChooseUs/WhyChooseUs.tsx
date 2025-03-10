e9dac2c6a5b0112855869968d3340d3412abeab1
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-10 17:27:36 -0400

    feat(home|WhyChooseUs): add desktop responsiveness to WhyChooseUs componenet

--- src/components/Pages/Home/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/Pages/Home/WhyChooseUs/WhyChooseUs.tsx
@@ -9,23 +9,29 @@ const WhyChooseUs = () => {
     <div className={styles.why_choose_us}>
       <div className='page__inner page__horizontal-spacing-3xl'>
         <div className={styles.content}>
-          <div className={styles.text_section}>
-            <h1 className='section-title'>Why Choose Bo Pro Power Washing?</h1>
-            <p className='section-paragraph'>
-              As a locally owned business with low overhead, we offer
-              high-quality power washing at competitive prices. We use safe,
-              professional-grade equipment to ensure your home looks its best,
-              and we don't consider a job done until you're fully satisfied.
-              With direct communication with the owner, you'll always receive
-              personalized service and transparent pricing with no hidden fees.
-            </p>
+          <div className={styles.top_section}>
+            <div className={styles.text_section}>
+              <h1 className='section-title'>
+                Why Choose Bo Pro Power Washing?
+              </h1>
+              <p className='section-paragraph'>
+                As a locally owned business with low overhead, we offer
+                high-quality power washing at competitive prices. We use safe,
+                professional-grade equipment to ensure your home looks its best,
+                and we don't consider a job done until you're fully satisfied.
+                With direct communication with the owner, you'll always receive
+                personalized service and transparent pricing with no hidden
+                fees.
+              </p>
+            </div>
+            <div className={styles.image}>
+              <img
+                src='/images/shared/brick-pressure-wash-large.webp'
+                alt='person power washing sidewalk'
+              />
+            </div>
           </div>
-          <div className={styles.image}>
-            <img
-              src='/images/shared/brick-pressure-wash-large.webp'
-              alt='person power washing sidewalk'
-            />
-          </div>
+
           <div className={styles.values}>
             {values.map(value => (
               <div className={`${styles.value} card`}>
