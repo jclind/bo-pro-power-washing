7a43f7ecc0f03412c360a7075377376c235aca69
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-10 16:26:33 -0400

    feat(home|WhyChooseUs): add WhyChooseUs component only mobile responsiveness

--- src/components/Pages/Home/WhyChooseUs/WhyChooseUs.tsx
+++ src/components/Pages/Home/WhyChooseUs/WhyChooseUs.tsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import styles from './WhyChooseUs.module.scss'
+import { values } from './values'
+import { FaRegCheckCircle } from 'react-icons/fa'
+import { ButtonLink } from '../../../Common/ButtonLink'
 
 const WhyChooseUs = () => {
-  return <div>WhyChooseUs</div>
+  return (
+    <div className={styles.why_choose_us}>
+      <div className='page__inner page__horizontal-spacing-3xl'>
+        <div className={styles.content}>
+          <div className={styles.text_section}>
+            <h1 className='section-title'>Why Choose Bo Pro Power Washing?</h1>
+            <p className='section-paragraph'>
+              As a locally owned business with low overhead, we offer
+              high-quality power washing at competitive prices. We use safe,
+              professional-grade equipment to ensure your home looks its best,
+              and we don't consider a job done until you're fully satisfied.
+              With direct communication with the owner, you'll always receive
+              personalized service and transparent pricing with no hidden fees.
+            </p>
+          </div>
+          <div className={styles.image}>
+            <img
+              src='/images/shared/brick-pressure-wash-large.webp'
+              alt='person power washing sidewalk'
+            />
+          </div>
+          <div className={styles.values}>
+            {values.map(value => (
+              <div className={`${styles.value} card`}>
+                <div className={styles.head}>
+                  <div className={styles.value_icon}>
+                    <FaRegCheckCircle className={styles.icon} />
+                  </div>
+                  <h2>{value.title}</h2>
+                </div>
+                <p className='section-paragraph'>{value.description}</p>
+              </div>
+            ))}
+          </div>
+          <ButtonLink
+            text="Let's Get Started"
+            link='/services'
+            color='tertiary'
+            size='large'
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default WhyChooseUs
---
+++ src/components/Pages/Home/WhyChooseUs/values.ts
@@ -0,0 +1,22 @@
+export const values = [
+  {
+    title: 'Locally Owned & Operated',
+    description:
+      " As a proud small business in Cranberry Township, we're committed to providing personalized, high-quality service. When you work with us, you get direct communication with the owner, ensuring reliability and attention to detail.",
+  },
+  {
+    title: 'High-Quality & Safe Cleaning',
+    description:
+      "We use professional-grade equipment and safe cleaning techniques to remove dirt, mold, and grime without damaging your property. Our approach ensures your home's surfaces, from siding to driveways, stay in excellent condition.",
+  },
+  {
+    title: 'Satisfaction Guaranteed',
+    description:
+      "Your satisfaction is our top priority, and we don't consider a job complete until you're happy with the results. We focus on quality workmanship, delivering a spotless, streak-free finish every time.",
+  },
+  {
+    title: 'Affordable, Transparent Pricing',
+    description:
+      "As a locally owned business with low overhead costs, we're able to offer competitive pricing without sacrificing quality. Our free estimates and upfront pricing ensure you get the best value with no hidden fees.",
+  },
+]
