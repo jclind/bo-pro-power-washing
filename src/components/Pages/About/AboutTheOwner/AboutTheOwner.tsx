ed68e98432b1a88981426dbff9f91cefb83bef3f
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-11 16:46:46 -0400

    feat(AboutTheOwner): add desktop responsiveness to AboutTheOwner component

--- src/components/Pages/About/AboutTheOwner/AboutTheOwner.tsx
+++ src/components/Pages/About/AboutTheOwner/AboutTheOwner.tsx
@@ -12,17 +12,19 @@ const AboutTheOwner = () => {
           </div>
           <div className={styles.text_section}>
             <h1 className='section-title text-inverted'>Meet The Owner</h1>
-            <p className='section-text text-inverted'>
-              Hi, I’m Bo , the owner of Bo Pro Power Washing. I started this
-              business because I love helping people take pride in their homes.
-              Whether it's a driveway covered in dirt or siding that needs a
-              refresh, I’m here to bring your property back to life with
-              professional power washing services
-            </p>
-            <p className='section-text text-inverted'>
-              Want to learn more about me or have any questions about Bo Pro
-              Power Washing?
-            </p>
+            <div className={styles.paragraphs}>
+              <p className='section-text text-inverted'>
+                Hi, I’m Bo , the owner of Bo Pro Power Washing. I started this
+                business because I love helping people take pride in their
+                homes. Whether it's a driveway covered in dirt or siding that
+                needs a refresh, I’m here to bring your property back to life
+                with professional power washing services
+              </p>
+              <p className='section-text text-inverted'>
+                Want to learn more about me or have any questions about Bo Pro
+                Power Washing?
+              </p>
+            </div>
             <ButtonLink link='/contact' text="Let's Talk" />
           </div>
         </div>
