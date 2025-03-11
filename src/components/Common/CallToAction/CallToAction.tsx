1b121e5cc559afeee6a555e219e1be20528e8547
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-11 15:19:41 -0400

    feat(CallToAction): create call to action component and design for mobile

--- src/components/Common/CallToAction/CallToAction.tsx
+++ src/components/Common/CallToAction/CallToAction.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import styles from './CallToAction.module.scss'
+import { ButtonLink } from '../ButtonLink'
 const CallToAction = () => {
-  return <div>CallToAction</div>
+  return (
+    <div className={styles.call_to_action}>
+      <div className='page__inner'>
+        <div className={`${styles.content} card`}>
+          <div className={styles.text_section}>
+            <h1>Let's Get In Touch</h1>
+            <p>
+              Ready to refresh your home? I can’t wait to hear from you—get your
+              free estimate today!
+            </p>
+            <ButtonLink text='Contact Us Today' link='/contact' />
+          </div>
+
+          <div className={styles.image}>
+            <img src='/images/shared/call-to-action-design.png' alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CallToAction
