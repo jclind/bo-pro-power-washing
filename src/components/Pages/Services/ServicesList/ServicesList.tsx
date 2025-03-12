2cab3a4bc4339d20b04cd1449ef7882c9054ce63
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-12 15:52:06 -0400

    feat(ServicesList): complete ServicesList for Mobile screens

--- src/components/Pages/Services/ServicesList/ServicesList.tsx
+++ src/components/Pages/Services/ServicesList/ServicesList.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
 import styles from './ServicesList.module.scss'
+import { servicesList } from './servicesList'
+import { ButtonLink } from '../../../Common/ButtonLink'
 
 const ServicesList = () => {
-  return <div>ServicesList</div>
+  return (
+    <div className={styles.services_list}>
+      <div className='page__inner page__horizontal-spacing-xl'>
+        <div className={styles.content}>
+          <div className={styles.services}>
+            {servicesList.map(service => (
+              <div className={`${styles.service} card`} key={service.title}>
+                <div className={styles.image}>
+                  <img src={service.img} alt='' />
+                </div>
+                <div className={styles.text_section}>
+                  <h2>{service.title}</h2>
+                  <div className={styles.divider}></div>
+                  <p className='section-paragraph'>{service.description}</p>
+                  <ButtonLink text='Get A Free Quote' link='/contact' />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ServicesList
