8c085c5a7a2219408e444d396455b15c8ad69a1c
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-03-13 19:14:36 -0400

    feat(projects): add hero to projects page and add general hero style classes

--- src/components/Pages/Projects/Hero/Hero.tsx
+++ src/components/Pages/Projects/Hero/Hero.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import styles from './Hero.module.scss'
 
 const Hero = () => {
-  return <div>Hero</div>
+  return (
+    <div className={styles.hero}>
+      <div className='page__inner hero-page'>
+        <h1 className='hero-title'>Past Projects</h1>
+        <p className='hero-description'>
+          Check out our past work for our loyal clients!
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Hero
--- src/components/Pages/Projects/ProjectsList/ProjectsList.tsx
+++ src/components/Pages/Projects/ProjectsList/ProjectsList.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './ProjectsList.module.css'
+import styles from './ProjectsList.module.scss'
 
 const ProjectsList = () => {
   return <div>ProjectsList</div>
